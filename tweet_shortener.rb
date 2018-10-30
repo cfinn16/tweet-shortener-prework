47835715d1fbee80cd6cd5a272d9d6ba38b18102
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-30 00:20:40 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def dictionary
+  {
+    :hello => "hi",
+    :to => "2",
+    :two => "2",
+    :too => "2",
+    :for => "4",
+    :four => "4",
+    :be => "b",
+    :you => "u",
+    :at => "a",
+    :and => "&",
+    }
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  tweet_array.map do |word|
+    if 
+      end
+    end
+   end
+   tweet_array.join(" ")
+end
+
